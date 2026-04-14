d49b1915562cf64ccf6db19bf0da1b4d0aee6813
Cherry Lai <clai@optix.com.hk>
2026-04-14 22:09:51 +0800

    Update config

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ load_dotenv()
 
 class Config:
     if os.environ.get('FLASK_ENV') == 'production':
-        SQLALCHEMY_DATABASE_URI = 'sqlite:////opt/render/project/src/data/music.db'
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     else:
         SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:Cherry5052005@localhost/Web_Ex"
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME', 'dmbzvxqe9')
-    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY', '865988435796388')
-    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET', '')
+    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
